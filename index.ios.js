c6ea4e09a7e4d9c70ed16a8d522634d54d0bda5c
iyisanahmet <iyisan.ahmet@gmail.com>
2019-04-11 00:59:45 +0300

    album details, card, cardsection

--- index.ios.js
+++ index.ios.js
@@ -1,13 +1,15 @@
 // IMPORT THE LIBRARIES
 import React from 'react';
 import { Text, View, AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 //CREATE COMPONENT
 const App = () => {
     return (
         <View>
             <Header headerText={'Albums'} />
+            <AlbumList />
         </View>
     );
 };
